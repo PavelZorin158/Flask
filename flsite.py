81728efd07264b5e260b44452922c3da85928b99
PavelZorin158 <pasharick@gmail.com>
2022-02-13 00:02:28 +0300

    добавлено горизонтальное меню

--- flsite.py
+++ flsite.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
 
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
 
 if __name__ == "__main__":
     app.run(debug=True)
