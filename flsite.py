3ae3a8f845c90b750d36282eaa9836bdaabe5379
PavelZorin158 <pasharick@gmail.com>
2022-02-13 16:56:44 +0300

    в flsite.py добавлено кучу обработчиков адресов. подправлен styles.css.

--- flsite.py
+++ flsite.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfghdfhg'
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
         {"name": "Обратная связь", "url": "contact"}]
-
-app = Flask(__name__)
 
 
 @app.route("/")
@@ -18,9 +19,41 @@ def about():
     print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
 
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    print(url_for('contact'))
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+    return render_template('contact.html', title="Обратная связь", menu=menu)
+
+
 @app.route("/profile/<username>")
 def profile(username):
-    return f"Пользователь: {username}"
+    print('username=', username)
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"Профиль пользователя: {username}"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'Rick' and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', titlr="Страница не найдена", menu=menu), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
